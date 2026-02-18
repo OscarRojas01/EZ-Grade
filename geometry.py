75938d6c9decd5ffc04751868ef5ea003362f836
OscarRojas01 <48222388+OscarRojas01@users.noreply.github.com>
2026-02-17 23:43:46 -0800

    Refactor slope calculation and interval creation functions
    Refactor slope calculation functions to improve clarity and functionality. Update to only handle sloping of z-axis across the x-axis and create linear intervals with cross slope application in separate function.

--- geometry.py
+++ geometry.py
@@ -1,60 +1,55 @@
-import math
+from typing import List, Tuple, Union
 # The script responsible for calculating slope and creating interval points
-# Seperate the handling of 2D & 3D slope calculations?
-# Allow for 2D slope calulation and apply a given cross-slope?
 
 
-def getSlope(point1, point2):
-    '''A function that takes a dictionary and touple to find the slope in both x & y directions from one point to another and returns the dictionary updated with the new slopes'''
+def get_slope(point1: dict, point2: Tuple):
+    '''A function that takes two points to find the slope, along ONLY the x-axis, from one point to another and adds the slope to the dictionary'''
 
-    distanceX = abs(point2[0] - point1['x'])
-    distanceY = abs(point2[1] - point1['y'])
-    elevationChange = point2[2] - point1['z']
-    slope = {
-        'x slope': (elevationChange/distanceX) / 2,
-        'y slope': (elevationChange/distanceY) / 2
-    }
-    return point1 | slope
+    distance = abs(point2[0] - point1['x'])
+    elevation_change = point2[2] - point1['z']
+    slope = round((elevation_change/distance), 4)
+    point1.update({'slope': slope})
 
 
-def applySlope(z, distance, slope):
+def apply_slope(z: Union[int, float], distance: Union[int, float], slope: Union[int, float]):
+    '''Applies given slope based on starting elevation and distance to next point, returns elevation'''
     return round(z + (distance * slope), 2)
 
-# def getNextPoint():
 
-
-def createIntervals(point1, point2, stepSize):
-    '''A function that creates interval points between a given dictionary and tuple, accounting for slope'''
-    intervalPoints = []
-    # Establishes how far back to trace through point list for the next elevation in y iteration
-    stepBack = math.ceil(abs(point1['x'] - point2[0]) / stepSize) + 1
-    for a in range(point1['y'], point2[1] + stepSize, stepSize):
-        if a == 0:
-            intervalPoints.append((point1['x'], point1['y'], point1['z']))
-        elif a > point2[1]:
-            prevPointElevation = intervalPoints[len(
-                intervalPoints) - stepBack][2]
-            point = (intervalPoints[len(intervalPoints) - stepBack][0], point2[1],
-                     applySlope(prevPointElevation, (a - stepSize), point1['y slope']))
-            intervalPoints.append(point)
+def create_linear_intervals(point1: dict, point2: List[Tuple], step_size: int):
+    '''A function that interpolates points between two known points (along the x-axis ONLY) with an unknown slope. Returns a list of points(x,y,z) stored as tuples'''
+    interval_points = []
+    get_slope(point1, point2)
+    for a in range(point1['x'], point2[0] + step_size, step_size):
+        if a == point1['x']:
+            interval_points.append((point1['x'], point1['y'], point1['z']))
+        elif a <= point2[0]:
+            prev_point_elevation = interval_points[len(interval_points) - 1][2]
+            point = (a, point1['y'], apply_slope(
+                prev_point_elevation, step_size, point1['slope']))
+            interval_points.append(point)
         else:
-            prevPointElevation = intervalPoints[len(
-                intervalPoints) - stepBack][2]
-            point = (intervalPoints[len(intervalPoints) - stepBack][0], a,
-                     applySlope(prevPointElevation, stepSize, point1['y slope']))
-            intervalPoints.append(point)
-        for b in range(point1["x"] + stepSize, point2[0] + stepSize, stepSize):
-            # Applies slope for each interval step through X-axis range along the current iteration of the Y-axis range
-            if b > point2[0]:
-                point = (point2[0], a, applySlope(
-                    intervalPoints[len(intervalPoints)-1][2], (b - point2[0]), point1['x slope']))
-                intervalPoints.append(point)
+            interval_points.append(point2)
+    return interval_points
+
+
+def apply_cross_slope(point_list: List[Tuple], width: int, cross_slope: Union[int, float], step_size: int):
+    '''Applies a cross slope to an existing line of points(in x,y,z format) to create a square area of points. Original list is updated'''
+    point_list_copy = point_list.copy()
+    point_list.clear()
+    for point_from_list in enumerate(point_list_copy):
+        point_list.append(point_from_list[1])
+        for b in range(0, width + step_size, step_size):
+            if b == 0:
+                continue
+            prev_point = point_list[-1]
+            if b <= width:
+                point = (point_from_list[1][0], b, apply_slope(
+                    prev_point[2], step_size, cross_slope))
             else:
-                point = (b, a, applySlope(
-                    intervalPoints[len(intervalPoints)-1][2], stepSize, point1['x slope']))
-                intervalPoints.append(point)
-    # displaying list for proof of accuracy
-    print(intervalPoints)
+                point = (point_from_list[1][0], width, apply_slope(
+                    prev_point[2], width - prev_point[1], cross_slope))
+            point_list.append(point)
 
 
 # test variables & function application
@@ -63,6 +58,9 @@ start = {
     'y': 0,
     'z': 0
 }
-end = (22, 60, 1.25)
-start = getSlope(start, end)
-createIntervals(start, end, 5)
+end = (22, 0, 1)
+CROSS_SLOPE = 0.02
+interval_point_list = create_linear_intervals(start, end, 5)
+print(interval_point_list)
+apply_cross_slope(interval_point_list, 22, CROSS_SLOPE, 5)
+print(interval_point_list)
